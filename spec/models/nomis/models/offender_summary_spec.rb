20872be966f99e0bb59f6104c4f5ff0cf28c9132
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-06-10 11:08:19 +0100

    Removes offenders from summary if no sentence start date
    When an offender is released, their sentence_start_date is set to nil,
    so this will cause a crash in the summary page because we can't
    determine their responsibility.
    
    This PR will exclude the offender from get_offenders_for_prison by
    claiming they are not sentenced if they have no sentence_start_date.

--- spec/models/nomis/models/offender_summary_spec.rb
+++ spec/models/nomis/models/offender_summary_spec.rb
@@ -1,20 +1,28 @@
 require 'rails_helper'
 
 describe Nomis::Models::OffenderSummary, model: true do
-  it "handles no earliest date" do
+  it 'handles no earliest date' do
     o = described_class.new
     o.sentence = Nomis::Models::SentenceDetail.new
     expect(o.sentence.earliest_release_date).to be_nil
     expect(o.sentenced?).to be false
   end
 
-  it "handles an earliest date" do
+  it 'handles an earliest date' do
     o = described_class.new.tap { |off|
       off.sentence = Nomis::Models::SentenceDetail.new
+      off.sentence.sentence_start_date = Date.new(2008, 1, 1)
       off.sentence.release_date = Date.new(2010, 1, 1)
       off.sentence.parole_eligibility_date = Date.new(2009, 1, 1)
     }
     expect(o.sentence.earliest_release_date).to eq(Date.new(2009, 1, 1))
     expect(o.sentenced?).to be true
+  end
+
+  it 'handles a case with no sentence start date' do
+    o = described_class.new.tap { |off|
+      off.sentence = Nomis::Models::SentenceDetail.new
+    }
+    expect(o.sentenced?).to be false
   end
 end
