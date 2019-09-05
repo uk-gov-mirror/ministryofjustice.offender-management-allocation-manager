eb7d8662c677cc36409a23856f105bf99eb7b4c9
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-09-05 12:04:32 +0100

    Handle missing primary_pom_allocated_at
    Sometimes, in an active allocation we have a missing
    primary_pom_allocated_at for historical reasons.  This causes to_date to
    fail when trying to find out when an allocation happened (so it can be
    assigned to the offender).
    
    In these cases, we will use the updated_at date as the allocation date,
    although more thoroughly we should probably iterate through the history
    to find the most recent primary pom allocated at. We are not doing this
    as we hope to fix up historical data by finding current allocation
    events with a missing primary_pom_allocated_at and update them.

--- spec/services/offender_service_spec.rb
+++ spec/services/offender_service_spec.rb
@@ -65,4 +65,32 @@ describe OffenderService do
     expect(updated_offenders.first.allocated_pom_name).to eq('Jones, Ross')
     expect(updated_offenders.first.allocation_date).to be_kind_of(Date)
   end
+
+  it "gets the POM names for allocated offenders with bad primary_pom_allocated_at",
+     vcr: { cassette_name: :offender_service_pom_names_date_fix_spec } do
+    offenders = described_class.get_offenders_for_prison('LEI').first(3)
+    nomis_staff_id = 485_752
+
+    PomDetail.create!(nomis_staff_id: nomis_staff_id, working_pattern: 1.0, status: 'active')
+
+    AllocationVersion.create!(
+      nomis_offender_id: offenders.first.offender_no,
+      nomis_booking_id: 1_153_753,
+      prison: 'LEI',
+      allocated_at_tier: 'C',
+      created_by_username: 'PK000223',
+      primary_pom_nomis_id: nomis_staff_id,
+      primary_pom_allocated_at: nil,
+      recommended_pom_type: 'prison',
+      event: AllocationVersion::ALLOCATE_PRIMARY_POM,
+      event_trigger: AllocationVersion::USER
+    )
+
+    updated_offenders = described_class.set_allocated_pom_name(offenders, 'LEI')
+    expect(updated_offenders).to be_kind_of(Array)
+    expect(updated_offenders.first).to be_kind_of(Nomis::Models::OffenderSummary)
+    expect(updated_offenders.count).to eq(offenders.count)
+    expect(updated_offenders.first.allocated_pom_name).to eq('Jones, Ross')
+    expect(updated_offenders.first.allocation_date).to be_kind_of(Date)
+  end
 end
