92bd4929c46b1e8cf8f6ff9e9aac88acbbdf1e31
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-12-12 09:39:31 +0000

    Refactor the hide and show functionality into functions and use them in the edit form by listening for the on_click event and then firing the hide or show function.
    We now can call the turbolinks:load event which will only be actioned
    within the case_information edit page following changes made to
    application.html.erb. If we are in the case_information edit page, once
    the page has loaded we will hide the rest of the form elements if the
    Scotland or Northern Ireland radio buttons have been checked.

--- app/assets/javascripts/case_info.js
+++ app/assets/javascripts/case_info.js
@@ -1,17 +1,15 @@
-$(document).ready(function () {
-  $('input[type="radio"][data-behavior="last-address"]').click(function (evt) {
-    if ($(this).val() == "Northern Ireland" || $(this).val() == "Scotland") {
-      $('.optional-case-info').hide();
-    } else {
-      $('.optional-case-info').show();
-    }
-  });
-});
+function hide_element() {
+    $('.optional-case-info').hide();
+}
 
-$(document).on('turbolinks:load', function(){
-    if((document.getElementById('case_information_probation_service_scotland').checked) || (document.getElementById('case_information_probation_service_northern_ireland').checked)){
-        $('.optional-case-info').hide();
-    }else if((document.getElementById('case_information_last_known_address_no').checked) || (document.getElementById('case_information_probation_service_wales').checked)) {
-        $('.optional-case-info').show();
+function show_element() {
+    $('.optional-case-info').show();
+}
+
+$(document).on("turbolinks:load", function(){
+    if ($(".case_information.edit").length > 0){
+        if((document.getElementById('case_information_probation_service_scotland').checked) || (document.getElementById('case_information_probation_service_northern_ireland').checked)){
+            hide_element();
+        }
     }
 });
